de03994531abc0a6ed4b61e0e5109d21445e9043
gguilt <58526116+naphteine@users.noreply.github.com>
2023-06-06 20:33:05 +0300

    Initial version with black screen

--- src/main.c
+++ src/main.c
@@ -1,4 +1,89 @@
+// Pong SDL2 by Radon Therapy
+// Initial version: 2014-10-08 05:11 - 2014-12-20 05:06
+// Rewrite: 2023-06-06 19:38 - 
+
+// Header files
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+// Variables and constants
+
+// - Window settings
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+// - SDL variables
+SDL_Window* gWindow = NULL;
+SDL_Renderer* gRenderer = NULL;
+
+// - Game variables
+bool gExit = false;
+
+// Functions
+bool game_init()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("[error] Couldn't initialize SDL2: %s\n", SDL_GetError());
+        return false;
+    }
+
+    gWindow = SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!gWindow) {
+        printf("[error] Couldn't create window: %s\n", SDL_GetError());
+        return false;
+    }
+
+    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+    if (!gRenderer) {
+        printf("[error] Couldn't create renderer: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+void game_exit()
+{
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWindow);
+    SDL_Quit();
+}
+
+void handle_input() {
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                gExit = true;
+                break;
+
+            default:
+                break;
+        }
+    }
+}
+
+void game_render()
+{
+    // Clean up screen
+    SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 255);
+    SDL_RenderClear(gRenderer);
+
+    // Render
+    SDL_RenderPresent(gRenderer);
+}
+
+// Main function
 int main(int argc, char* argv[])
 {
-    return 0;
+    game_init();
+
+    while(!gExit) {
+        handle_input();
+        game_render();
+    }
+
+    game_exit();
 }
